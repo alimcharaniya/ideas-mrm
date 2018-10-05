8fc511d0e470f4065c9b4ddc3acca8fcd51af769
Tommy Nguyen <tommynguyen@Tommys-MacBook-Pro.local>
2018-10-04 19:09:40 -0700

    added memberSID object and student ID field to the existing member page

--- src/app/components/existing-member/existing-member.component.ts
+++ src/app/components/existing-member/existing-member.component.ts
@@ -9,7 +9,9 @@ import { Component } from '@angular/core';
 export class ExistingMemberComponent {
   title = 'app';
 
-
+  myobject = {
+    memberSID: "",
+  }
 
   newMember(){
     alert("existing");
